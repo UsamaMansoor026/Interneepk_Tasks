255c995345570f2936501118e7cfa82e090d218d
Usama Mansoor <usamamansoor026@gmail.com>
2024-07-02 21:27:58 +0500

    Nothing special to commit

--- TSK_000_42/src/pages/SignUp.jsx
+++ TSK_000_42/src/pages/SignUp.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <section className="container">
+      <h3>Welcome to my Website</h3>
+
+      <form>
+        <input type="text" placeholder="Enter Your Name..." required />
+        <input type="email" placeholder="Enter Your Email..." required />
+        <input type="password" placeholder="Enter Your Password..." required />
+        <input
+          type="password"
+          placeholder="Confirm Your Password..."
+          required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </section>
+  );
 };
 
 export default SignUp;
